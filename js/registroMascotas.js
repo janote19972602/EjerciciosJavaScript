652c7c599fab40515d2a0f50dcf892f31ac93304
Alejandro <alebkn2602@gmail.com>
2025-10-01 17:44:10 -0300

    se logra programar el boton calcular estadistica con una funcion

--- js/registroMascotas.js
+++ js/registroMascotas.js
@@ -27,12 +27,93 @@ window.onload = function () {
 
     btnCalcularEstadistica.addEventListener('click', function () {
 
-        verificarPerroGato();
-        
+        //verificarPerroGato(mascotasArreglo);
+        //realizarCalculoDeMascotas(mascotasArreglo);
+        calculoTotalDeMascotas(mascotasArreglo);
+        console.log("entro");
+
         
     })
 
+
+
+}
+
+//     // 2. Inicializa variables para almacenar las edades
+//     let gatoMasJoven = Infinity;
+//     let gatoMasViejo = -Infinity;
+//     let perroMasJoven = Infinity;
+//     let perroMasViejo = -Infinity;
+
+// function realizarCalculoDeMascotas(mascotasArreglo) {
+
+//     let resultadoH2 = obtenerResultadoH2Mascota();
+
+//     for (let i = 0; i < mascotasArreglo.length; i++) {
+//         const animal = mascotasArreglo[i]; //Acceder al elemento actual del array
+
+//         if (animal.tipo === "gato" || animal.tipo === "Gato") {
+//             if (animal.edad < gatoMasJoven) {
+//                 gatoMasJoven = animal.edad;
+//             }
+//             if (animal.edad > gatoMasViejo) {
+//                 gatoMasViejo = animal.edad;
+//             }
+//             else if (animal.tipo === "perro" || animal.tipo === "Perro") {
+//                 if (animal.edad < perroMasJoven) {
+//                     perroMasJoven = animal.edad;
+//                 }
+//                 if (animal.edad > perroMasViejo) {
+//                     perroMasViejo = animal.edad;
+//                 }
+//             }
+//         }
+        
+//     }
+
+//         console.log(`Gato más joven: ${gatoMasJoven} años`);
+//         console.log(`Gato más viejo: ${gatoMasViejo} años`);
+//         console.log(`Perro más joven: ${perroMasJoven} años`);
+//         console.log(`Perro más viejo: ${perroMasViejo} años`);
+
+//         // 5. Llama a la función para analizar los animales
+//         realizarCalculoDeMascotas(mascotasArreglo);
     
+// }
+
+   
+
+
+
+function calculoTotalDeMascotas(mascotasArreglo) {
+
+    console.log(mascotasArreglo);
+    
+
+    let resultadoH2 = obtenerResultadoH2Mascota();
+
+    let cantidadDePerros = 0;
+    let cantidadDeGatos = 0;
+    
+
+    for (let i = 0; i < mascotasArreglo.length; i++) {
+        
+        
+        let mascotas = mascotasArreglo[i];
+        mascotas.tipo;
+        console.log(mascotas);
+
+        if (mascotas.tipo === "perro" || mascotas.tipo === "Perro" || mascotas.tipo === "perra" || mascotas.tipo === "Perra") {
+            cantidadDePerros++;
+            console.log("hola");
+            
+        }else if (mascotas.tipo === "gato" || mascotas.tipo === "Gato" || mascotas.tipo === "gata" || mascotas.tipo === "Gata") {
+            cantidadDeGatos++;
+        }
+    }
+
+    resultadoH2.textContent = `cantidad total de perros:${cantidadDePerros} y la cantidad de gatos: ${cantidadDeGatos}`;
+    //console.log(cantidadDePerros);
     
 }
 
@@ -40,30 +121,27 @@ window.onload = function () {
 function verificarPerroGato(mascotasArreglo) {
 
     
-    let cantidad = obtenerResultadoH2Mascota();
+    let resultadoH2 = obtenerResultadoH2Mascota();
 
     let cantidadPerros = 0; //se inicializa el contador en 0
+    let cantidadGatos = 0;  //se inicializa el contador en 0
 
     //repetimos sobre cada mascota en el arreglo
 
     for (let i = 0; i < mascotasArreglo.length; i++) {
         let mascotaActual = mascotasArreglo[i];
+        mascotaActual.tipo;
 
-        if (mascotaActual.tipo === "perro") {
-            cantidad.textContent = `esta mascota se llama ${mascotaActual.nombre} y es un perro`;
+        if (mascotaActual.tipo === "Perro" || mascotaActual.tipo === "perro" || mascotaActual.tipo === "Perra" || mascotaActual.tipo === "perra") {
             cantidadPerros++;
-        }else if (mascotaActual.tipo === "gato") {
-            cantidad.textContent = `esta mascota es un ${mascotaActual.nombre} y es un gato`;
+        }else if (mascotaActual.tipo === "gato" || mascotaActual.tipo === "Gato" || mascotaActual.tipo === "gata" || mascotaActual.tipo === "Gata") {
+            cantidadGatos++;
         }
 
-        cantidad.textContent = `en total hay ${cantidadPerros} en la lista`;
-
-
+        resultadoH2.textContent = `En total hay ${cantidadPerros} perros y ${cantidadGatos} gatos en la lista`;
+        console.log(resultadoH2);
         
     }
-
-
-    
 }
 
 //en este proyecto deberan haber 4 funciones para obtener el valor
